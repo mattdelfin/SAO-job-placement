0e9a3f5e13d216bc3c85bf9c803880ff6af9ed64
Kimkim787 <alcantaraclement@gmail.com>
2024-05-12 05:13:42 +0800

    new

--- SAOapp/jobplacement/urls.py
+++ SAOapp/jobplacement/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
 from . import views
+
+app_name='jobplacement'
 urlpatterns = [
-    path('', views.jobMain, name="home"),
+    path('', views.mainpage),
+    path('main/', views.jobMain, name="home"),
+
+    path('jobplacement/report/', views.transRep, name="job_trans_rep"),
 ]
--- SAOapp/jobplacement/views.py
+++ SAOapp/jobplacement/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+
+    return render(request, 'jobplacement/main.html', {})
+
 def jobMain(request):
 
     return render(request, 'jobplacement/jobMain.html', {})
+
+def transRep(request):
+
+    return render(request, 'jobplacement/trans_report.html', {})
