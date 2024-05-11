d956b94e8c59d810392ca3fd2657d0900196466e
Kimkim787 <alcantaraclement@gmail.com>
2024-05-12 05:30:18 +0800

    New Transaction and Issuance Files

---
+++ SAOapp/runserver.bat
@@ -0,0 +1,4 @@
+@echo on
+CALL C:\Users\alcan\OneDrive\Desktop\2nd Year\INTEGRATIVE PROGRAMING\DJANGO FINAL PROJECT\SAOapp\env\Scripts\activate.bat
+python manage.py runserver
+TIMEOUT /t -1 /NOBREAK
